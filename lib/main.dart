e23b429c79fe4616ba100420758aaef51f39a82b
oereo <dlstpgns0406@gmail.com>
2023-11-08 03:07:49 +0900

    feat: onboarding view example

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:doonut/services/theme_service.dart';
 import 'package:doonut/views/home_view.dart';
+import 'package:doonut/views/on_boarding_view.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeView(),
+      home: OnBoardingView(),
     );
   }
 }
